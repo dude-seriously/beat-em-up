93fc63c8c9ee5c2151922ec3e7629ba8e9090991
Maksims Mihejevs <contact@maxm.co>
2013-02-10 16:49:13 +0000

    click

--- client/js/input.js
+++ client/js/input.js
@@ -121,6 +121,20 @@ function Input() {
   });
 }
 
+$(window).bind('touchstart', function preventZoom(e) {
+  var t2 = e.timeStamp
+    , t1 = $(this).data('lastTouch') || t2
+    , dt = t2 - t1
+    , fingers = e.originalEvent.touches.length;
+  $(this).data('lastTouch', t2);
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault(); // double tap - prevent the zoom
+  // also synthesize click events we just swallowed up
+  $(this).trigger('click').trigger('click');
+});
+
+
 Input.prototype.horiz = function() {
   return this.right - this.left;
 }
